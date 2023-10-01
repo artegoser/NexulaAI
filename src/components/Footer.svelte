374efa0b7ec2ee4fc1c1d136866c39e0abc09d3b
Artemy <artegoser@gmail.com>
2023-10-01 11:23:51 +0300

    feat: send message

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,23 @@
 <script>
 	import { getContext } from 'svelte';
 	import { Icon, PaperAirplane } from 'svelte-hero-icons';
+
+	import { db } from '$lib/db';
+
 	let currentTab = 0;
 
 	let state = getContext('state');
+
+	$: addMessage = async () => {
+		if ($state.tab === 'chat') {
+			await db.chat_messages.add({
+				chat_id: $state.currentTabItem,
+				role: 'user',
+				text: $state.text,
+				time: Date.now()
+			});
+		}
+	};
 </script>
 
 <div class="m-4">
@@ -11,8 +25,13 @@
 		<div class="flex flex-row">
 			<div class="basis-1/4" />
 			<div class="basis-1/2 flex items-start">
-				<textarea class="textarea" rows="2" placeholder="Send a message." value={$state.text} />
-				<button type="button" class="btn variant-filled m-2">
+				<textarea
+					class="textarea"
+					rows="2"
+					placeholder="Send a message."
+					bind:value={$state.text}
+				/>
+				<button type="button" class="btn variant-filled m-2" on:click={addMessage}>
 					Send <Icon src={PaperAirplane} class="w-6 h-6 m-2" />
 				</button>
 			</div>
--- src/components/chatBubble.svelte
+++ src/components/chatBubble.svelte
@@ -9,17 +9,27 @@
 	export let message;
 </script>
 
-<div class="grid grid-cols-[auto_1fr] gap-2">
-	<Avatar
-		initials={role}
-		width="w-12"
-		background={role === 'user' ? 'bg-red-600' : 'bg-green-600'}
-	/>
-	<div class="card p-4 variant-soft rounded-tl-none space-y-2">
+<div class={`grid ${role === 'user' ? 'grid-cols-[1fr_auto]' : 'grid-cols-[auto_1fr]'} gap-2`}>
+	{#if role !== 'user'}
+		<Avatar
+			initials={role}
+			width="w-12"
+			background={role === 'user' ? 'bg-red-600' : 'bg-green-600'}
+		/>
+	{/if}
+
+	<div class={`card p-4 ${role === 'user' ? 'rounded-tr-none' : 'rounded-tl-none'} space-y-2`}>
 		<header class="flex justify-between items-center">
 			<p class="font-bold">{role?.toLocaleUpperCase()}</p>
 			<small class="opacity-50">{new Date(time).toLocaleString()}</small>
 		</header>
 		<p>{message}</p>
 	</div>
+	{#if role === 'user'}
+		<Avatar
+			initials={role}
+			width="w-12"
+			background={role === 'user' ? 'bg-red-600' : 'bg-green-600'}
+		/>
+	{/if}
 </div>
